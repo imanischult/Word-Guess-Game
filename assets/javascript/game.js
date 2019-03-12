a20322ff02c346f6b133707c7de2ccaf5562d9e0
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-03-12 01:19:59 -0400

    Generates variables for word guess game

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Create an array which includes a list of words for game //
+var words = ["Nigeria", "Ethiopia", "Egypt", "DRCongo", "Tanzania", "SouthAfrica", "Kenya", "Uganda", "Algeria", "Sudan", "Chad", "Guinea", "Rwanda", "Burundi", "SierraLeone"]
+
+// Create a formula that picks a random integer between 0 and 14 //
+var random = Math.floor(Math.random()*15);
+
+// Create a formula that picks a random index from "words" array //
+var answer = words[random];
+
+var wordLength = answer.length;
+var display=[wordLength];
+var 
+
+
